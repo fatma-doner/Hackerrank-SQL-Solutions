77da9870a1fc5075148e215e09a535abe33335e8
Fatma Doner <fatmadoner72@gmail.com>
2025-08-04 23:18:36 +0300

    Add solution to problem The Report on Hackerrank

---
+++ The_Report_Solution.sql
@@ -0,0 +1,17 @@
+SELECT 
+    CASE 
+        WHEN g.GRADE >= 8 THEN s.NAME
+        ELSE 'NULL'
+    END AS NAME,
+    g.GRADE,
+    s.MARKS
+FROM 
+    STUDENTS s
+JOIN 
+    GRADES g ON s.MARKS BETWEEN g.MIN_MARK AND g.MAX_MARK
+ORDER BY 
+    g.GRADE DESC,
+    CASE 
+        WHEN g.GRADE >= 8 THEN s.NAME
+        ELSE TO_CHAR(s.MARKS)
+    END ASC;
